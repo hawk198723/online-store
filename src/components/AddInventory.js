1af4be7e44fcc601e7bdc9d6edd7cccfd9228755
Jason Wang <jason@Jasons-MBP.attlocal.net>
2022-07-11 13:21:30 -0700

    submit form ongoing

--- src/components/AddInventory.js
+++ src/components/AddInventory.js
@@ -1,11 +1,111 @@
 import React, { Component } from "react";
 
 class AddInventory extends Component {
+  state = {
+    name: "",
+    price: "",
+    tags: "",
+    image: "",
+    status: "available",
+  };
+
+  handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  submit = (e) => {
+    e.preventDefault();
+    const product = { ...this.state };
+    console.log(product);
+  };
+
   render() {
     return (
       <div className="inventory">
         <p className="title has-text-centered">Inventory</p>
-        <br />
+        <form onSubmit={this.submit}>
+          <div className="field">
+            <div className="control">
+              <label className="label">Name</label>
+              <textarea
+                className="textarea"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <label className="label">Price</label>
+              <input
+                type="number"
+                className="input"
+                name="price"
+                value={this.state.price}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <label className="label">Tags</label>
+              <input
+                type="text"
+                className="input"
+                name="tags"
+                value={this.state.tags}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <label className="label">Image</label>
+              <input
+                type="text"
+                className="input"
+                name="image"
+                value={this.state.image}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <label className="label">Status</label>
+              <div className="select is-fullwidth">
+                <select
+                  name="status"
+                  value={this.state.status}
+                  onChange={this.handleChange}
+                >
+                  <option>available</option>
+                  <option>unavailable</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <br />
+
+          <div className="field is-grouped is-grouped-centered">
+            <div className="control">
+              <button className="button is-link">Submit</button>
+            </div>
+            <div className="control">
+              <button className="button is-link">Cancel</button>
+            </div>
+          </div>
+          {/* <br />
         <div className="control">
           <input type="text" className="input" />
         </div>
@@ -18,7 +118,8 @@ class AddInventory extends Component {
           >
             Cancel
           </button>
-        </div>
+        </div> */}
+        </form>
       </div>
     );
   }
