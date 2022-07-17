5a1340976388798014058d5f81c49bdb460c0336
Jason Wang <jason@Jasons-MBP.attlocal.net>
2022-07-17 13:41:31 -0700

    done w/ update and delete cartItem

--- src/components/CartIem.js
+++ src/components/CartIem.js
@@ -1,13 +1,35 @@
 import { formatPrice } from "commonFunctions/helper";
-import React from "react";
+import React, { useState } from "react";
+import axios from "commonFunctions/axios";
 
 const CartIem = (props) => {
-  const { name, image, price, amount } = props.cart || {};
+  const [amount, setAmount] = useState(props.cart.amount);
+  const { id, name, image, price } = props.cart || {};
   const sumPrice = formatPrice(amount * parseInt(price));
+
+  const handleChange = (e) => {
+    const _amount = parseInt(e.target.value);
+    setAmount(_amount);
+    const newCart = {
+      ...props.cart,
+      amount: _amount,
+    };
+    axios.put(`/carts/${id}`, newCart).then((res) => {
+      props.updateCart(newCart);
+    });
+  };
+
+  const deleteCart = () => {
+    axios.delete(`/carts/${id}`).then((res) => {
+      props.deleteCart(props.cart);
+    });
+  };
   return (
     <div className="columns is-vcentered">
       <div className="column is-narrow">
-        <span className="close">X</span>
+        <span className="close" onClick={deleteCart}>
+          ✖︎
+        </span>
       </div>
       <div className="column is-narrow">
         <img src={image} alt={name} width="100" />
@@ -20,7 +42,9 @@ const CartIem = (props) => {
         <input
           type="number"
           className="input number-input"
-          defaultValue={amount}
+          min={1}
+          value={amount}
+          onChange={handleChange}
         />
       </div>
       <div className="column">
